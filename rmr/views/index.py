bd25c225a6f8adb89e371ad700aab3957d7e0f00
Haneef Khan <khanhak@umich.edu>
2023-08-08 18:35:45 -0400

    add redirect from /input/

--- rmr/views/index.py
+++ rmr/views/index.py
@@ -8,3 +8,7 @@ def show_index():
     context = {}
     #connection = rmr.model.get_db()
     return flask.render_template("index.html", **context)
+
+@app.app.route('/input/')
+def show_input_form():
+    return flask.redirect("https://docs.google.com/forms/d/e/1FAIpQLSfoHfv6Y3raKQe6h_Leab6Db76E-PVN5cJDvKVfExbRpf0waQ/viewform",code=302)
